9eac08e11ea91ef2835a0b89ef2db51282562f68
Egor <lobanow.egor@outlook.com>
2025-05-10 16:42:02 +0300

    Добавил функционал классам Boat и Oar.

--- src/Boat.py
+++ src/Boat.py
@@ -4,11 +4,51 @@ from src.Oar import Oar
 class Boat:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.passengers = 0
         self.speed = 0.0
-        self.direction = 0.0
+        self.passengers = 0
+
+        #Высчитываем ускорение с помощью пассажиров, больше вместимость -> больше начальное ускорение
+        self.speed_increment = 1 + capacity / 10 # чем меньше пассажиров тем больше будет скорость
         self.sinking = False
+        self.water_level = 0.0
         self.oars = {
-            "left": Oar(2.5, 'wood'),
-            "right": Oar(2.5, 'wood')
+            "left": Oar(2.5, 2),
+            "right": Oar(2.5, 2)
         }
+
+    def row(self):
+        if self.sinking:
+            raise RuntimeError('Мы тонем!')
+
+        #Погружаем оба весла в воду
+        self.oars['left'].in_water = True
+        self.oars['right'].in_water = True
+
+
+        if not self.oars['left'].is_turned and not self.oars['right'].is_turned:
+            if self.speed <= 6: #Не даём превысить среднюю скорость весельной лодки
+                self.speed += self.speed_increment # Добавляем скорости
+            else:
+                self.speed = 6
+        else: # Если вёсла развёрнуты, то скорость будет снижаться
+            if self.speed >= 0: # Не даём уйти в отрицательную скорость
+                self.speed -= self.speed_increment
+            else:
+                self.speed = 0
+
+        self.oars['left'].in_water = False
+        self.oars['right'].in_water = False
+
+    def embark(self):
+        if self.passengers + 1 < self.capacity: # Одно место занимает тот кто гребёт
+            self.passengers += 1
+            self.speed_increment -= 1 - self.passengers / 10
+        else:
+            raise ValueError('Места кончились!')
+
+    def disembark(self):
+        if self.passengers - 1 >= 0:
+            self.passengers -= 1
+            self.speed_increment += 1 - self.passengers / 10
+        else:
+            raise ValueError('Количество пассажиров не может быть меньше нуля!')
--- src/Oar.py
+++ src/Oar.py
@@ -1,5 +1,9 @@
 class Oar:
-    def __init__(self, length, material):
-        self.length = length
-        self.material = material
+    def __init__(self, length, weight):
+        self.length = length #m
+        self.weight = weight #kg
         self.in_water = False
+        self.is_turned = False # Если развернуть весло углублением вперёд -> начнётся торможение
+
+    def turn_oar(self):
+        self.is_turned = not self.is_turned
